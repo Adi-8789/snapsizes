23814d7b1294c482314f9d2536fe394b868fd451
Aditya shankar <shankaraditya980@gmail.com>
2026-03-17 21:20:30 +0530

    Add Monetag service worker and fix ESLint error

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
+/* global importScripts */
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 220745
+    "zoneId": 10743362  
 }
 self.lary = ""
-// eslint-disable-next-line no-undef
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
